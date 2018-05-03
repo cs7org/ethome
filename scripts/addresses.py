69f03832f7d9d151b3146ccef6da7956ae15ec19
jsschl <jonas.schlund@gmail.com>
2018-05-03 13:34:28 +0200

    added addresses.py

---
+++ scripts/addresses.py
@@ -0,0 +1,10 @@
+#replace with your corresponding addresses
+
+#contracts:
+CommunityController = '0x8CfbeF64C750164705592623Df2E5c448B31A6f2'
+
+#nodes:
+alpha = '686fc8f10aa31860b6dc9d5bfeb8cdb9e9ba2db7'
+beta = 'cf46ce6bac05e6a00dabf0d0187afcedcb97957a'
+delta = '29620127a0ba7ea609455c3f922c74341be90713'
+gamma =	'90f6fb1d0e87d14d10c413d827a4efc046b43671'
